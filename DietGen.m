04d1c75199b550d4ff09b80e326c75c2e8ab4cee
AnyuSHAN <anyushan2004@gmail.com>
2025-01-21 19:36:02 +0000

    Added automatic meal generation and bolus insulin calculation, updated definition of simulation time

---
+++ DietGen.m
@@ -0,0 +1,53 @@
+function [time_vector, CHO_vector] = DietGen(start_time, end_time)
+% The distribution of diet is adapted from Garry et. al (2006)
+
+% Initialize output vectors
+relative_times = [];
+meal_amounts = [];
+
+% Loop through each day in the range
+current_date = dateshift(start_time, 'start', 'day'); % Get thwee first day's start
+while current_date <= end_time
+    % Generate meal pattern for the current day (example function call)
+    [meal_times, meal_amounts_day] = generate_meal_pattern();
+    
+    % Adjust meal times relative to the overall start_time
+    time_offset = minutes(current_date - start_time); % Offset in minutes from start_time
+    relative_meal_times = meal_times + time_offset;
+    
+    % Append results to the output vectors
+    relative_times = [relative_times; relative_meal_times];
+    meal_amounts = [meal_amounts; meal_amounts_day];
+    
+    % Move to the next day
+    current_date = current_date + days(1);
+end
+
+% round the meal times and exclude entries smaller than relative time 1
+relative_times = round(relative_times);
+
+valid_indices = (relative_times >= 1) & (relative_times < minutes(end_time - start_time));
+time_vector = relative_times(valid_indices);
+CHO_vector = meal_amounts(valid_indices);
+end
+
+function [meal_times, meal_amounts] = generate_meal_pattern()
+    % Generate random meal times
+    meal_times = [8; 13; 18; 22]*60; % Times in minutes from midnight (8 AM, 1 PM, 6 PM, 10PM)
+    meal_times = meal_times + randn(length(meal_times), 1)*30; % Add variability to meal time
+
+    % Generate randome meal amounts
+    meal_amounts = [
+        87.9 + 11.5*randn; 
+        69.0 + 8.8*randn; 
+        45.3 + 7.7*randn;
+        55.1 + 8.4*randn
+    ];   % Corresponding meal amounts (grams of carbs, etc.)
+end
+
+
+% Reference:
+% Garry M. Steil, Kerstin Rebrin, Christine Darwin, Farzam Hariri, 
+% Mohammed F. Saad; Feasibility of Automating Insulin Delivery for the 
+% Treatment of Type 1 Diabetes. Diabetes 1 December 2006; 55 (12): 3344–3350. 
+% https://doi.org/10.2337/db06-0419
--- init.m
+++ init.m
@@ -1,9 +1,12 @@
+clear;
+clc;
+
 %% ===== Simulation Setup =====
 
 % Definition of session time
 
-start_time = datetime(2025, 1, 21, 6, 30, 0); % Format: year, month, day, hour, minute, second
-end_time = datetime(2025, 1, 21, 23, 15, 0);  % Example: Same day, different time
+start_time = datetime(2025, 1, 21, 0, 30, 0); % Format: year, month, day, hour, minute, second
+end_time = datetime(2025, 1, 22, 23, 30, 0);  % Example: Same day, different time
 
 % Calculate the duration between the times
 t_sim = minutes(end_time - start_time);
@@ -30,14 +33,36 @@ basal_data = [0, U_b0; 1440*5, U_b0];
 
 %% ===== Input Definition =====
 
-% insulin dosage are all given in mU
-bolus_data = ConvertPWL([120], [2e3]);
+control_flag = false;
+prandial_flag = true;
 
-% glucagon dosage are all given in pg
-glucagon_data = ConvertPWL([360], [1e8]);
+if control_flag
+    % insulin dosage are all given in mU
+    bolus_data = ConvertPWL([120], [2e3]);
+    
+    % glucagon dosage are all given in pg
+    glucagon_data = ConvertPWL([360], [1e8]);
+    
+    % CHO intake are defined in grams of glucose intake
+    CHO_data = ConvertPWL([120], [40]);
+else
+    % Randomize and construct meal disturbances
+    [CHO_time, CHO_amount] = DietGen(start_time, end_time);
+    CHO_data = ConvertPWL(CHO_time, CHO_amount);
 
-% CHO intake are defined in grams of glucose intake
-CHO_data = ConvertPWL([120], [40]);
+    if prandial_flag
+        ICR = 1e3/15; % Insulin-CHO ratio assumed to be 1000mU per 15g CHO
+        bolus_amount = CHO_amount.*(1+0.1*randn(size(CHO_amount)))*ICR;
+        bolus_data = ConvertPWL(CHO_time, bolus_amount);
+    else
+        bolus_data = [0, 0; 1e6, 0];
+    end
+
+    glucagon_data = [0, 0; 1e6, 0];
+end
+
+
+%% ===== Auxilary Functions =====
 
 function LoadVP(dir)
 % This function reads Virtual Patient Parameters from cohort and load

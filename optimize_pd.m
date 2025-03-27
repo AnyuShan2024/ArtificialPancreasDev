f154189555cc11d026c85c330fdf80381eff9cc3
AnyuSHAN <anyushan2004@gmail.com>
2025-03-27 22:14:27 +0800

    Minor modification to tuning scripts

--- optimize_pd.m
+++ optimize_pd.m
@@ -2,11 +2,11 @@
 model_name = 'SimBG'; % Replace with your actual Simulink model name
 
 % Define the parameters to optimize
-param_names = {'K_d', 'K_p'}; % Two parameters for tuning
+param_names = {'K_d_glu', 'K_p_glu'}; % Two parameters for tuning
 
 % Define the lower and upper bounds for each parameter
 param_bounds = [0, 0;  % Lower bounds (gd, gp)
-                20,  1];   % Upper bounds (gd, gp)
+                5,  5];   % Upper bounds (gd, gp)
 
 % Run the Genetic Algorithm optimizer
 optimal_params = paramTune(model_name, param_names, param_bounds);
